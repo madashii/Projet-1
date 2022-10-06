1a97cd90f7c6539134e4cacadd8a5a32725f1dc1
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2022-10-06 16:23:48 +0200

    avant le pull carousel

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,14 +1,12 @@
-const menuPokeball = document.querySelector(".menu-poke")
-    const navLinks = document.querySelector(".nav-links")
+// const menuPokeball = document.querySelector(".menu-poke")
+//     const navLinks = document.querySelector(".nav-links")
 
-    menuPokeball.addEventListener('click',()=>{
-    navLinks.classList.toggle('mobile-menu')
-    })
+//     menuPokeball.addEventListener('click',()=>{
+//     navLinks.classList.toggle('mobile-menu')
+//     })
+const animationLogoNav = document.querySelector(".logo-menu-navbar");
+// const displayList = document.querySelectorby(".container-navbar-ul");
 
-
-
-
-   
-
-
-  
+animationLogoNav.addEventListener("click", () => {
+  animationLogoNav.classList.toggle("rotation-image-nav");
+});
