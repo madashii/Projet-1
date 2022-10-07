a43b0624f299f500b7e96768aa08077b1b137a3e
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2022-10-07 10:12:51 +0200

    before la fusion

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -5,11 +5,8 @@ const displayList = document.querySelector("#nav-list");
 animationNavLogo.addEventListener("click", () => {
   animationNavLogo.classList.toggle("nav-image-rotation");
   displayList.classList.toggle("nav-list-display");
-  console.log("ok");
 });
 // animation navigation article scroll smooth
-const ancreNav = document.querySelectorAll("ancre-action-nav");
-
-ancreNav.addEventListener("click", () => {
-  ancreNav.forEach((element) => console.log("ok"));
+displayList.addEventListener("click", () => {
+  animationNavLogo.classList.toggle("nav-image-rotation");
 });
